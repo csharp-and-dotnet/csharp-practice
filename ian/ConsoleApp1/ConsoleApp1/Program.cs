605a915033cced1707aa7621f18cddc9735c3e40
iangameplayhans <iangameplayhans@gmail.com>
2025-11-27 23:31:33 -0400

    create proyect basic csharp

---
+++ ian/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 10;
+int b = 11;
+int c = a + b;
+Console.WriteLine(a+","+b+","+c);
+
+for(int i = 0; i < a; i++)
+{
+    Console.WriteLine(i);
+}
+
